a667f43b2eb371e9be54192891824e74666e0a7c
robotian <robotian@gmail.com>
2023-09-14 11:13:09 -0400

    tf time delay error

--- src/tf2pose_publisher.cpp
+++ src/tf2pose_publisher.cpp
@@ -12,7 +12,7 @@
 #include <geometry_msgs/msg/pose_stamped.hpp>
 #include "../tf2_geometry_msgs/tf2_geometry_msgs/tf2_geometry_msgs.hpp"
 
-using namespace std::chrono_literals;
+// using namespace std::chrono_literals;
 
 class TF2PosePulisher : public rclcpp::Node
 {
@@ -24,24 +24,37 @@ class TF2PosePulisher : public rclcpp::Node
       target_frame_ = this->declare_parameter<std::string>("target_frame", "rov1");
       tf_buffer_ = std::make_unique<tf2_ros::Buffer>(this->get_clock());
       tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
-      pose_publisher_ = this->create_publisher<geometry_msgs::msg::PoseStamped>("pose_topic", 10);
+      pose_publisher_ = this->create_publisher<geometry_msgs::msg::PoseStamped>("pose", 10);
       // Call on_timer function every second
-      timer_ = this->create_wall_timer(1ms, std::bind(&TF2PosePulisher::on_timer, this));
+      // timer_ = this->create_wall_timer(500ms, std::bind(&TF2PosePulisher::on_timer, this));
+
+      timer_ = create_wall_timer(
+      std::chrono::milliseconds(1000/20), [this]
+      {
+        on_timer();
+      });
       
     }
 
   private:
+    // Parameters
+    // BaseContext cxt_;
+    rclcpp::Duration transform_expiration_{std::chrono::milliseconds{0}};
     void on_timer()
     {
+      
       // Store frame names in variables that will be used to
       // compute transformations
       std::string fromFrameRel = target_frame_.c_str();
       std::string toFrameRel = "world";
 
       geometry_msgs::msg::TransformStamped transform;
-
+      
       try {
-            transform = tf_buffer_->lookupTransform(toFrameRel, fromFrameRel,tf2::TimePointZero);
+        if(tf_buffer_->canTransform(toFrameRel,fromFrameRel,tf2::TimePointZero)){
+          transform = tf_buffer_->lookupTransform(toFrameRel, fromFrameRel,tf2::TimePointZero);
+        }
+            
       } catch (const tf2::TransformException & ex) {
         RCLCPP_INFO(
           this->get_logger(), "Could not transform %s to %s: %s",
@@ -49,8 +62,9 @@ class TF2PosePulisher : public rclcpp::Node
         return;
       }
 
+
       geometry_msgs::msg::PoseStamped pose_msg;
-      pose_msg.header.stamp = this->get_clock()->now();
+      
       pose_msg.header.frame_id = "world";
       pose_msg.pose.position.x = 0.0;
       pose_msg.pose.position.y = 0.0;
@@ -60,8 +74,29 @@ class TF2PosePulisher : public rclcpp::Node
       pose_msg.pose.orientation.z = 0.0;
       pose_msg.pose.orientation.w = 1.0;
 
+      // RCLCPP_INFO(this->get_logger(),"the current pose frame id is %s",pose_msg.header.frame_id.c_str() );
       tf2::doTransform(pose_msg, pose_msg, transform);
+
+      transform_expiration_ = {std::chrono::milliseconds{100}};
+      // baselink to left_camera static tf hardcoding
+      // 'translation.x': -0.15,
+      // 'translation.y': 0.18,
+      // 'translation.z': -0.0675,
+      // 'rotation.x': 0.0,
+      // 'rotation.y': 0.7071067811865475,
+      // 'rotation.z': 0.0,
+      // 'rotation.w': 0.7071067811865476,
+      pose_msg.header.stamp = now() + transform_expiration_;
       pose_msg.header.frame_id = "world";
+      // pose_msg.pose.position.x += -0.15;
+      // pose_msg.pose.position.y += 0.18;
+      // pose_msg.pose.position.z += -0.0675;
+      // pose_msg.pose.orientation.x += 0.0;
+      // pose_msg.pose.orientation.y += 0.7071067811865475;
+      // pose_msg.pose.orientation.z += 0.0;
+      // pose_msg.pose.orientation.w += 0.7071067811865476;
+
+      // RCLCPP_INFO(this->get_logger(),"the pose frame id is %s",pose_msg.header.frame_id.c_str() );
 
       // RCLCPP_INFO(this->get_logger(), "Try to publish");
       pose_publisher_->publish(pose_msg);
