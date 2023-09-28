c2cbd57ca259acaf838fb1b8ed0099ab7141561a
robotian <robotian@gmail.com>
2023-09-28 17:10:38 -0400

    parameterize frame names

--- src/tf2pose_publisher.cpp
+++ src/tf2pose_publisher.cpp
@@ -14,6 +14,10 @@
 
 // using namespace std::chrono_literals;
 
+// Parameters
+// target_tf : the frame name that you want to publish as a pose topic
+// fixed_tf : the frame name that target_tf is relative to.
+
 class TF2PosePulisher : public rclcpp::Node
 {
   public:
@@ -21,9 +25,23 @@ class TF2PosePulisher : public rclcpp::Node
     : Node("tf2pose_publisher")
     {
       // Declare and acquire `target_frame` parameter
-      target_frame_ = this->declare_parameter<std::string>("target_frame", "rov1");
+      this->declare_parameter<std::string>("target_tf", "rov");
+      this->declare_parameter<std::string>("fixed_tf", "map");
+
+      // parameter update
+      target_frame_ = this->get_parameter("target_tf").as_string();
+      fixed_frame_ = this->get_parameter("fixed_tf").as_string();
+
+
+      RCLCPP_INFO(this->get_logger(), "target_tf --> %s", target_frame_.c_str());
+      RCLCPP_INFO(this->get_logger(), "fixed_tf --> %s", fixed_frame_.c_str());
+
+      std::vector<rclcpp::Parameter> all_new_parameters{rclcpp::Parameter("target_tf", target_frame_.c_str()), rclcpp::Parameter("fixed_tf", fixed_frame_.c_str())};
+      this->set_parameters(all_new_parameters);
+
       tf_buffer_ = std::make_unique<tf2_ros::Buffer>(this->get_clock());
       tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
+
       pose_publisher_ = this->create_publisher<geometry_msgs::msg::PoseStamped>("pose", 10);
       // Call on_timer function every second
       // timer_ = this->create_wall_timer(500ms, std::bind(&TF2PosePulisher::on_timer, this));
@@ -40,13 +58,15 @@ class TF2PosePulisher : public rclcpp::Node
     // Parameters
     // BaseContext cxt_;
     rclcpp::Duration transform_expiration_{std::chrono::milliseconds{0}};
+    
+
     void on_timer()
     {
       
       // Store frame names in variables that will be used to
       // compute transformations
       std::string fromFrameRel = target_frame_.c_str();
-      std::string toFrameRel = "world";
+      std::string toFrameRel = fixed_frame_.c_str();
 
       geometry_msgs::msg::TransformStamped transform;
       
@@ -61,11 +81,11 @@ class TF2PosePulisher : public rclcpp::Node
           toFrameRel.c_str(), fromFrameRel.c_str(), ex.what());
         return;
       }
-
+      // rclcpp::Parameter()
 
       geometry_msgs::msg::PoseStamped pose_msg;
       
-      pose_msg.header.frame_id = "world";
+      pose_msg.header.frame_id = toFrameRel;
       pose_msg.pose.position.x = 0.0;
       pose_msg.pose.position.y = 0.0;
       pose_msg.pose.position.z = 0.0;
@@ -87,7 +107,7 @@ class TF2PosePulisher : public rclcpp::Node
       // 'rotation.z': 0.0,
       // 'rotation.w': 0.7071067811865476,
       pose_msg.header.stamp = now() + transform_expiration_;
-      pose_msg.header.frame_id = "world";
+      // pose_msg.header.frame_id = "world";
       // pose_msg.pose.position.x += -0.15;
       // pose_msg.pose.position.y += 0.18;
       // pose_msg.pose.position.z += -0.0675;
@@ -108,6 +128,7 @@ class TF2PosePulisher : public rclcpp::Node
     rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr pose_publisher_;
     std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
     std::string target_frame_;
+    std::string fixed_frame_;
 };
 
 int main(int argc, char * argv[])
